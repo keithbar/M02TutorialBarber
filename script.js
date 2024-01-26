c43f6385d7fd584e39b1c46ddb9caecea1febbf7
Keith <kbarber34@ivytech.edu>
2024-01-26 15:30:44 -0500

    added map and footer

---
+++ script.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoia2JhcmJlcjM0IiwiYSI6ImNscnYyejJzbDBuNzEya2xlbnd0cjdoOTIifQ.pGT3UvQqmp75GNLsT_cKjA';
+  var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [-71.060982, 42.35725],
+    zoom: 18
+  });
